e21eade7197b29a64e180eb3514a4cd46b62cf82
Erin Kavanaugh <erin.kavanaugh@capitalone.com>
2017-04-18 23:31:27 -0700

    Searches for Barry's but doesn't get the correct li selector

--- classes.js
+++ classes.js
@@ -1,18 +1,26 @@
 var Nightmare = require('nightmare'); 
 var nightmare = Nightmare({show: true}); 
-
+var cheerio = require('cheerio');
+var studio = "Barry's Bootcamp - SoMa"
 nightmare.goto('https://classpass.com/classes')
          .wait('.filter__section')
-         // .click('.filter__select ')
+         .wait('.filter__header__arrow')
+         .click('.filter__section:nth-child(5) .filter__header__arrow')
+         .wait('.ng-empty')
+         .type('.ng-empty', "Barry's Bootcamp")
+         .wait(`.filter__option--~ .filter__option+ .filter__option a[text="${studio}"]`)
+         .click(`.filter__option--~ .filter__option+ .filter__option a[text="${studio}"]`)
+         // .click('.filter__select')
          // .click('.filter__section :nth-child(2) > a')
          // .wait('.filter__search > input[type="text"]')
          // .type('.filter__search > input[type="text"]')
          .evaluate(function() {
-           return document.querySelectorAll('.filter__section')
+           return document.body.innerHTML
          })
          .end()
-         .then(function(data) {
-          console.log(data)
+         .then(function(body) {
+            var $ = cheerio.load(body); 
+          // console.log(body)
          })
          .catch(function(err) {
           console.log(err)
---
+++ user-info.js
@@ -0,0 +1,4 @@
+module.exports = {
+  email: 'elkavanaugh@gmail.com',
+  password: 'spinerette'
+}
